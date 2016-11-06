a4c81389e36a8ff4b1793cf46d51558f09f1ab98
Yury Ivanov <yvi1@bu.edu>
2016-11-05 22:20:41 -0400

    removed the unnecessary dependency gson

--- src/main/java/org/cidarlab/owldispatcher/adaptors/PigeonClient.java
+++ src/main/java/org/cidarlab/owldispatcher/adaptors/PigeonClient.java
@@ -6,8 +6,6 @@ import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
 public class PigeonClient {
-	
-	//private static String pigeonCode = "p p1 2%0D%0Ar r2 3%0d%0ag g3 6%0D%0At t4 13%0d%0a# Arcs";
 
 	public static String requestPigeon(String pigeonCodeEncoded) throws UnirestException{
 		HttpResponse<String> response = Unirest.post("http://synbiotools.bu.edu:5801/dev/perch2.php")
