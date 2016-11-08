1b1f67a8059a95dc514c2c9d23fa8f583af9e7a7
PrashantVaidyanathan <prash@bu.edu>
2016-11-08 17:56:18 -0500

    indentation OCD

--- src/test/java/org/cidarlab/owldispatcher/adaptors/LatexAdaptorTest.java
+++ src/test/java/org/cidarlab/owldispatcher/adaptors/LatexAdaptorTest.java
@@ -16,34 +16,29 @@ import org.junit.Test;
  * @author prash
  */
 public class LatexAdaptorTest {
-    
-    
-    
+
     @Test
-    public void testLatexFileCreation(){
+    public void testLatexFileCreation() {
         OwlData owl = new OwlData();
         owl.addPigeonFilepath("Monocistronic_prgt_1", Utilities.getOutputFilepath() + "new-test2" + Utilities.getFileDivider() + "Monocistronic_prgt_1.png");
         owl.addPigeonFilepath("Monocistronic_prgt_2", Utilities.getOutputFilepath() + "new-test2" + Utilities.getFileDivider() + "Monocistronic_prgt_2.png");
         owl.addPigeonFilepath("Monocistronic_prgt_3", Utilities.getOutputFilepath() + "new-test2" + Utilities.getFileDivider() + "Monocistronic_prgt_3.png");
         owl.setMyProjectId("new-test2");
-        
-        	LatexAdaptor.getLatexFilepath(owl);
-        	
-            String pathToWorkDir = Utilities.getOutputFilepath()+owl.getMyProjectId();
-            String pathToTex = pathToWorkDir+Utilities.getFileDivider()+owl.getMyProjectId();
-        	
-        	ShellExec exec = new ShellExec(true, false);
-            try {
-    			exec.execute("pdflatex", pathToWorkDir, true, pathToTex);
-    		} catch (IOException e) {
-    			e.printStackTrace();
-    		}
-            System.out.println(exec.getOutput());
 
-        
-       
-        
+        LatexAdaptor.getLatexFilepath(owl);
+
+        String pathToWorkDir = Utilities.getOutputFilepath() + owl.getMyProjectId();
+        String pathToTex = pathToWorkDir + Utilities.getFileDivider() + owl.getMyProjectId();
+
+        ShellExec exec = new ShellExec(true, false);
+        try {
+            exec.execute("pdflatex", pathToWorkDir, true, pathToTex);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(exec.getOutput());
+
     }
-    
-    
+
 }
+
